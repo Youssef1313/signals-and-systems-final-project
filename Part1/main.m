7e02a714274408e42c9963ccc42365299092ad9b
Youssef Victor <youssefvictor00@gmail.com>
2021-01-01 15:35:20 +0200

    Enhancements to part 1

--- Part1/main.m
+++ Part1/main.m
@@ -9,15 +9,24 @@ y1 = y_func(3 * t);
 y2 = y_func(t + 2);
 y3 = y_func(4 - 2 * t);
 
+figure('Name','Question 1','NumberTitle','off');
 subplot(2, 2, 1);
 plot(t, y);
+xlabel('t');
+ylabel('y(t)');
 subplot(2, 2, 2);
 plot(t, y1);
+xlabel('t');
+ylabel('y(3t)');
 subplot(2, 2, 3);
 plot(t, y2);
+xlabel('t');
+ylabel('y(t + 2)');
 subplot(2, 2, 4);
 plot(t, y3);
-figure;
+xlabel('t');
+ylabel('y(4 - 2t)');
+
 
 % Question 2
 t_min = -500;
@@ -26,6 +35,15 @@ t = generate_samples(t_min, t_max, sampling_freq);
 m = sinc(0.01 * t) .* sinc(0.01 * t);
 T = fftshift(fft(t));
 F = linspace(-sampling_freq /2, sampling_freq /2, (t_max - t_min) * sampling_freq);
-plot(t, m);
-figure;
-plot(F, abs(T));
+magnitude = abs(T);
+phase = angle(T);
+figure('Name','Question 2 - a - F.T. of (sinc(10^-3 * t))^2','NumberTitle','off');
+subplot(3, 1, 1);
+plot(F, magnitude);
+title('Magnitude graph');
+subplot(3, 1, 2);
+plot(F(49980:50021), magnitude(49980:50021));
+title('Magnitude graph - zoomed in');
+subplot(3, 1, 3);
+plot(F, phase);
+title('Phase graph');
