a125ac2d71926146d1a69d643e66c7fd0eeb19cb
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2021-01-12 20:16:22 +0200

    Some fixes (#3)
    * Some fixes
    
    * Update Part1/main.m

--- Part1/main.m
+++ Part1/main.m
@@ -1,6 +1,6 @@
-% Question 1
+%% Question 1
 
-sampling_freq = 1000;
+sampling_freq = 200;
 timeSamples = generate_samples(-5, 5, sampling_freq);
 syms t;
 y_func = exp(abs(t) / -5) * (heaviside(t + 1) - heaviside(t - 3));
@@ -28,22 +28,28 @@ plot(timeSamples, y3);
 xlabel('t');
 ylabel('y(4 - 2t)');
 
-
-% Question 2
-timeSamples = generate_samples(-2000, 2000, sampling_freq);
+%% Question 2
+timeSamples = generate_samples(-1, 1, sampling_freq);
 m = sinc(0.001 * timeSamples) .^ 2;
 M = fftshift(fft(m));
-F = linspace(-sampling_freq /2, sampling_freq /2, length(timeSamples));
-magnitude = abs(M);
-phase = angle(M);
+F = linspace(-sampling_freq / 2, sampling_freq / 2, length(timeSamples));
 figure('Name', 'Question 2 - a - F.T. of (sinc(10^-3 * t))^2', 'NumberTitle', 'off');
-subplot(3, 1, 1);
-plot(F, magnitude);
+subplot(2, 1, 1);
+plot(F, abs(M));
 title('Magnitude graph');
+subplot(2, 1, 2);
+plot(F, angle(M));
+title('Phase graph');
+
+r = m .* cos(2 * pi * 100000 * timeSamples);
+figure('Name', 'Question 2 - b', 'NumberTitle', 'off');
+subplot(3, 1, 1);
+plot(timeSamples, r);
+title('r(t)');
+R = fftshift(fft(r));
 subplot(3, 1, 2);
-middle_index = length(F) / 2;
-plot(F(middle_index-10:middle_index+10), magnitude(middle_index-10:middle_index+10));
-title('Magnitude graph - zoomed in');
+plot(F, abs(R));
+title('Magnitude graph');
 subplot(3, 1, 3);
-plot(F, phase);
+plot(F, angle(R));
 title('Phase graph');
